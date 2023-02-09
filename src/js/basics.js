3788b3f789715cc5cc34214934953ba619a9fab5
spanic <sadulin2011@yandex.ru>
2023-02-10 00:42:28 +0600

    adds default create-react-app structure with Prettier and first JS task + Jest tests

---
+++ src/js/basics.js
@@ -0,0 +1,16 @@
+function sum(...numbers) {
+  if (!Array.isArray(numbers) || !numbers.length) {
+    throw new Error('Please add at least one argument');
+  }
+  return numbers.reduce((result, currentValue) => {
+    if (isNaN(currentValue)) {
+      throw new Error(
+        `Argument '${currentValue}' cannot be converted to number, please check the inputs`,
+      );
+    } else {
+      return result + Number(currentValue);
+    }
+  }, 0);
+}
+
+module.exports = sum;
---
+++ src/js/basics.test.js
@@ -0,0 +1,27 @@
+const sum = require('./basics');
+
+describe('Validating sum() function', () => {
+  test('works fine with numbers and/or convertible data', () => {
+    expect(sum(1, 2)).toBe(3);
+    expect(sum(1, '+2.3', '-3', '3')).toBe(3.3);
+    expect(sum(0)).toBe(0);
+    expect(sum(true, false)).toBe(1);
+    expect(sum(null, '0', -10.99)).toBe(-10.99);
+  });
+
+  test('throws with no agruments', () => {
+    expect(() => sum()).toThrowError(/please add at least one argument/i);
+  });
+
+  test('throws with non-convertible values', () => {
+    [
+      expect(() => sum(undefined)),
+      expect(() => sum(null)),
+      expect(() => sum(1, 'UH%42', 3)),
+    ].every((value) =>
+      value.toThrowError(
+        /^argument '.*' cannot be converted to number, please check the inputs$/i,
+      ),
+    );
+  });
+});
