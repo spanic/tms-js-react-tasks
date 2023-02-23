e74233702cf7e4d234fa4405deab9c30e4ec4367
ViShiZa <Shizajob@gmail.com>
2023-02-23 15:57:00 +0300

    Added lesson 2 homework for review.

--- src/js/lesson_2/homework.js
+++ src/js/lesson_2/homework.js
@@ -18,6 +18,30 @@
  * 3 3 3 3
  * @param {number} height total number of rows (i. e. height of the tree)
  */
-function christmasTree(height) {}
+function christmasTree(height) {
+  let tree = '';
 
+  if (
+    typeof height !== 'number' ||
+    height % 1 !== 0 ||
+    height < 1 ||
+    height > 10
+  ) {
+    throw new Error(
+      `Argument '${height}' is invalid, expected positive integer from 1 to 10`
+    );
+  } else {
+    for (let i = 0; i < height; i++) {
+      tree += ' '.repeat(height - 1 - i) + `${i}`;
+      for (let k = 1; k <= i; k++) {
+        tree += ` ${i}`;
+      }
+      tree += ' '.repeat(height - 1 - i) + '\n';
+    }
+  }
+  console.log(tree);
+  return tree;
+}
+
+christmasTree(3);
 export { christmasTree };
