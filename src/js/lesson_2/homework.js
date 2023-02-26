df3d3b8adc158c23b35fca29db8b31fd23f5ccec
ViShiZa <Shizajob@gmail.com>
2023-02-26 12:47:58 +0300

    Adds corrections to 2 homework, the first part.

--- src/js/lesson_2/homework.js
+++ src/js/lesson_2/homework.js
@@ -23,24 +23,61 @@ function christmasTree(height) {
 
   if (
     typeof height !== 'number' ||
-    height % 1 !== 0 ||
+    !Number.isInteger(height) ||
     height < 1 ||
     height > 10
   ) {
-    throw new Error(
-      `Argument '${height}' is invalid, expected positive integer from 1 to 10`
-    );
-  } else {
-    for (let i = 0; i < height; i++) {
-      tree += ' '.repeat(height - 1 - i) + `${i}`;
-      for (let k = 1; k <= i; k++) {
-        tree += ` ${i}`;
-      }
-      tree += ' '.repeat(height - 1 - i) + '\n';
-    }
+    throw new Error(`Argument '${height}' is invalid, expected positive integer from 1 to 10`);
   }
-  console.log(tree);
+  for (let i = 0; i < height; i++) {
+    tree += ' '.repeat(height - 1 - i) + `${i}`;
+    for (let k = 1; k <= i; k++) {
+      tree += ` ${i}`;
+    }   
+    tree += ' '.repeat(height - 1 - i);
+    if (i < height -1){
+      tree += '\n';
+    };
+  }
+  
   return tree;
 }
 
-export { christmasTree };
+/**
+ * Функция должна возвращать копию исходного массива, содержащего только числа, отсортированную в порядке убывания (от большего к меньшему).
+ * __Внимание__: в рамках данной задачи нельзя использовать фунции ``` Array.prototype.sort() ``` и ``` Array.prototype.reverse() ```
+ *
+ * Реализуйте проверку параметра ``` array ```:
+ *
+ * если аргумент функции не является массивом, нужно завершить выполнение функции с ошибкой:
+ * ``` throw new Error('Argument '{значение_аргумента}' is not an array, expected to be an array of numbers'); ```
+ *
+ * если хотя бы один из элементов исходного массива не является числом / равен NaN, нужно завершить выполнение фунции с ошибкой:
+ * ``` throw new Error('Element '{значение_элемента}' cannot be coerced to number / is NaN, expected to be a number'); ```
+ *
+ * Подсказки:
+ * 1. Для создания копии массива используйте ``` const copy = [...input]; ```
+ * 2. Для проверки входного параметра используйте ``` Array.isArray(input); ```
+ * 3. Для проверки типа данных элементов используйте предоставленную вложенную функцию
+ *
+ * @example
+ * [1, 2, 3, 4, 5] => [5, 4, 3, 2, 1]
+ *
+ * @param {array} array array of numbers
+ * @returns {array} copy of the input array, sorted in reverse order
+ */
+function reverseSort(array) {
+  if (!Array.isArray(array)){
+    throw new Error(`Argument '${array}' is not an array, expected to be an array of numbers`);
+  }
+
+  function checkIsNumber(value) {
+    if (typeof value !== 'number' || Number.isNaN(value)) {
+      throw new Error(
+        `Element '${value}' cannot be coerced to number / is NaN, expected to be a number`
+      );
+    }
+  }
+}
+
+export { christmasTree, reverseSort };
