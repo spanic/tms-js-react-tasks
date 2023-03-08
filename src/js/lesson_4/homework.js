f87819189f4ffac5cf20849c9d956daefe66e5cc
ViShiZa <Shizajob@gmail.com>
2023-03-08 19:34:53 +0300

    Add homework 4 to check.

--- src/js/lesson_4/homework.js
+++ src/js/lesson_4/homework.js
@@ -1,4 +1,4 @@
-// import movies from '../data/moviesTestData';
+import movies from '../data/moviesTestData';
 
 /**
  * Функция должна возвращать топ-N фильмов с самым большим бюджетом. Если фильмов с одинаковым бюджетом несколько, они все должны попасть в результат выполнения функции.
@@ -18,10 +18,34 @@
  * @returns array of movies ordered by Production_Budget desc (массив фильмов, отсортированных по значению Production_Budget в порядке убывания (от большего к меньшему))
  */
 function getMaxProductionBudgetMovies(amount) {
-  
-  movies.sort((a, b) => b.Production_Budget - a.Production_Budget);
+  const sortedMovies = [];
 
-  const sortedMovies = movies.slice(0, amount);
+  while (amount > 0) {
+    const previousMaxBudget =
+      sortedMovies[sortedMovies.length - 1]?.Production_Budget;
+
+    let sameBudget = [];
+    let maxBudget = -Infinity;
+
+    for (let i = 0; i < movies.length; i++) {
+      const movie = movies[i];
+      const currentBudget = movie.Production_Budget;
+
+      if (currentBudget === null || isNaN(currentBudget)) continue;
+
+      if (previousMaxBudget && currentBudget >= previousMaxBudget) continue;
+
+      if (currentBudget > maxBudget) {
+        maxBudget = currentBudget;
+        sameBudget = [movie];
+      } else if (currentBudget === maxBudget) {
+        sameBudget.push(movie);
+      }
+    }
+
+    amount -= 1;
+    sortedMovies.push(...sameBudget);
+  }
 
   return sortedMovies;
 }
@@ -35,16 +59,28 @@ function getMaxProductionBudgetMovies(amount) {
  * @returns map ``` {Major_Genre: [movies]} ``` where movies are sorted by ``` IMDB_Rating ``` and ``` Rotten_Tomatoes_Rating ``` desc
  */
 function getMoviesGroupedByGenres() {
-  console.log(movies[0].Major_Genre);
 
   movies.sort((a, b) =>
     (b.IMDB_Rating > a.IMDB_Rating) - (a.IMDB_Rating > b.IMDB_Rating)
     || (b.Rotten_Tomatoes_Rating > a.Rotten_Tomatoes_Rating) - (a.Rotten_Tomatoes_Rating > b.Rotten_Tomatoes_Rating));
 
-  const sortedMovies = [...movies];
+  const sortedMovies = {};
 
-  console.log(sortedMovies);
+  movies.forEach((movie) => {
 
+    const currentGenre = movie.Major_Genre || movie.Creative_Type;
+
+    if (!currentGenre) return;
+
+    if (sortedMovies[currentGenre]) {
+      sortedMovies[currentGenre].push(movie);
+    } else {
+      sortedMovies[currentGenre] = [movie];
+    }
+  });
+
+
+  return sortedMovies;
 }
 
 export { getMaxProductionBudgetMovies, getMoviesGroupedByGenres };
