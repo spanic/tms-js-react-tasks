42c6b717fa3a464bae8b478d5cb484e8e2d1d9ea
maxordi <max.ordashevskiy@yandex.by>
2023-03-11 15:53:57 +0300

    my_lesson3

--- src/js/lesson_3/homework.js
+++ src/js/lesson_3/homework.js
@@ -28,6 +28,9 @@
 function User(id, name) {
   this.id = id;
   this.name = name;
+
+  this.compareById = compareById;
+  this.compareByNameLength = compareByNameLength;
 }
 
 /**
@@ -36,7 +39,7 @@ function User(id, name) {
  * @param {object} object object to be compared with (ссылка на сравниваемый объект)
  */
 function compareById(object) {
-  // ...
+  return compare.call(this, object, (object) => object.id, compareIds);
 }
 
 /**
@@ -45,29 +48,49 @@ function compareById(object) {
  * @param {object} object object to be compared with (ссылка на сравниваемый объект)
  */
 function compareByNameLength(object) {
-  // ...
+  return compare.call(
+    this, 
+    object, 
+    (object) => object.name?.length, 
+    compareNameLengths
+  );
 }
 
-// function compare(object, getPropToCompareFn, compareFn) {
-//   return compareFn(getPropToCompareFn(this), getPropToCompareFn(object));
-// }
+function compare(object, getPropToCompareFn, compareFn) {
+  return compareFn(getPropToCompareFn(this), getPropToCompareFn(object));
+}
 
 /**
  * Эта функция должна реализовывать логику сравнения значений id и возвращать искомый результат
  * @param {number} firstValue initial object's id (значение id исходного объекта)
  * @param {number} secondValue comparable object's id (значение id сравниваемого объекта)
  */
-// function compareIds(firstValue, secondValue) {
-//   // ...
-// }
+function compareIds(firstValue, secondValue) {
+  if (firstValue > secondValue) {
+    return ` Object 1 is greater than Object 2: id ${firstValue} > ${secondValue} `;
+  } else if (firstValue === secondValue) {
+    return ` Object 1 is equal to Object 2: id ${firstValue} == ${secondValue} `;
+  } else {
+    return ` Object 1 is less than Object 2: id ${firstValue} < ${firstValue} `;
+  }
+}
 
 /**
  * Эта функция должна реализовывать логику сравнения значений name.length и возвращать искомый результат
  * @param {number} firstValue initial object's name length (значение длины name исходного объекта)
  * @param {number} secondValue comparable object's name length (значение длины name сравниваемого объекта)
  */
-// function compareNameLengths(firstValue, secondValue) {
-//   // ...
-// }
+
+
+
+function compareNameLengths(firstValue, secondValue) {
+  if (firstValue > secondValue) {
+    return ` Object 1 is less than Object 2: name length ${firstValue} > ${secondValue} `;
+  } else if (firstValue === secondValue) {
+    return ` Object 1 is equal to Object 2: name length ${firstValue} == ${secondValue} `;
+  } else {
+    return ` Object 1 is greater than Object 2: name length ${firstValue} < ${firstValue} `;
+  }
+}
 
 export { User, compareById, compareByNameLength };
