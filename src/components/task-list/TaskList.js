1f8d1f36536a4c15a78a96c5a04267240359a2e0
ViShiZa <Shizajob@gmail.com>
2023-03-26 10:19:04 +0300

    Add homework 6 to check.

--- src/components/task-list/TaskList.js
+++ src/components/task-list/TaskList.js
@@ -34,22 +34,89 @@ function TaskList() {
     /**
      * "Enter your task here..." input
      */
-    // ...
+    const addsInput = document.createElement('input');
+    
+    addsInput.classList.add('task-list__input');
+    addsInput.setAttribute('type', 'text');
+    addsInput.setAttribute('placeholder', 'Enter your task here...');
+
+    const parentDivElement = document.querySelector('div.task-list__row');
+    const refElement = parentDivElement.lastElementChild;
+
+    parentDivElement.insertBefore(addsInput, refElement);
 
     /**
      * "Total:" div
      */
-    // ...
+    const addsDivTotal = document.createElement('div');
+    const addsTextNote = document.createTextNode('Total: ');
+    const addsSpanTotal = document.createElement('span');
+    const addsTextNoteTotal = document.createTextNode('2');
 
+    addsDivTotal.append(addsTextNote);
+    addsDivTotal.append(addsSpanTotal);
+    addsSpanTotal.append(addsTextNoteTotal);
+
+    const parentElement = document.querySelectorAll('div.task-list__row');
+    const childDiv = parentElement[1].querySelector('div');
+
+    parentElement[1].insertBefore(addsDivTotal, childDiv);
+    
+    
     /**
      * "Show completed" button
      */
-    // ...
+    const addsButton = document.createElement('button');
+    const addsTextNoteButton = document.createTextNode('Show completed');
+
+    addsButton.append(addsTextNoteButton);
+    addsButton.classList.add('task-list__button');
+    addsButton.setAttribute('disabled', '');
+
+    const childInput = parentElement[1].querySelector('input');
+    parentElement[1].insertBefore(addsButton, childInput);
+
 
     /**
      * First task item (<div className="task-list__item">)
      */
-    // ...
+    
+    const addsParentDivTaskListItem = document.createElement('div');
+    const addsDivTaskListItemShevron = document.createElement('div');
+    const addsSpanTaskListItem = document.createElement('span');
+    const addsTextNoteSpanTaskListItem = document.createTextNode('Occaecat adipisicing dolor labore eu nostrud amet fugiat. Occaecat dolor aute mollit aute. Ad laborum elit Lorem exercitation non proident excepteur cupidatat deserunt ut cillum sit. Cupidatat quis sint velit ea consequat minim.');
+    const addsDivTaskListItemControls = document.createElement('div');
+    const addsDivTaskListItemRow = document.createElement('div');
+    const addsButtonTaskListItemComplete = document.createElement('button');
+    const addsTextNoteButtonTaskListItemComplete = document.createTextNode('Complete');
+    const addsButtonTaskListItemRemove = document.createElement('button');
+    const addsSpanTaskListItemCreatedDate = document.createElement('span');
+    const addsTextNodeSpanTaskListItemCreatedDate = document.createTextNode('02.03.2023 00:13 GMT+6');
+
+    addsParentDivTaskListItem.classList.add('task-list__item');
+    addsParentDivTaskListItem.append(addsDivTaskListItemShevron);
+    addsDivTaskListItemShevron.classList.add('task-list-item__chevron');
+    addsParentDivTaskListItem.append(addsSpanTaskListItem);
+    addsSpanTaskListItem.append(addsTextNoteSpanTaskListItem);
+    addsParentDivTaskListItem.append(addsDivTaskListItemControls);
+    addsDivTaskListItemControls.classList.add('task-list-item__controls');
+    addsDivTaskListItemControls.append(addsDivTaskListItemRow);
+    addsDivTaskListItemRow.classList.add('task-list__row');
+    addsDivTaskListItemRow.append(addsButtonTaskListItemComplete);
+    addsButtonTaskListItemComplete.classList.add('task-list__button', 'task-list__button_complete');
+    addsButtonTaskListItemComplete.append(addsTextNoteButtonTaskListItemComplete);
+    addsDivTaskListItemRow.append(addsButtonTaskListItemRemove);
+    addsButtonTaskListItemRemove.classList.add('task-list__button', 'task-list__button_remove');
+    addsDivTaskListItemControls.append(addsSpanTaskListItemCreatedDate);
+    addsSpanTaskListItemCreatedDate.classList.add('task-list-item__created-date');
+    addsSpanTaskListItemCreatedDate.append(addsTextNodeSpanTaskListItemCreatedDate);
+
+
+    const parentDivTaskListContainer = document.querySelector('div.task-list__container');
+    const firstChildTaskListContainer = parentDivTaskListContainer.firstChild;
+
+    parentDivTaskListContainer.insertBefore(addsParentDivTaskListItem, firstChildTaskListContainer);
+ 
   }, []);
 
   return (
@@ -64,26 +131,26 @@ function TaskList() {
           </button>
         */}
         {/* Добавьте вручную: */}
-        <input
+        {/* <input
           className="task-list__input"
           type="text"
           placeholder="Enter your task here..."
-        ></input>
+        ></input> */}
         <button className="task-list__button">Add</button>
       </div>
       <div className="task-list__row">
         {/* Добавьте вручную: */}
-        <div>
+        {/* <div>
           Total: <span>2</span>
-        </div>
+        </div> */}
         <div>
           Completed: <span>1</span>
         </div>
         <button className="task-list__button">Show all</button>
         {/* Добавьте вручную: */}
-        <button className="task-list__button" disabled>
+        {/* <button className="task-list__button" disabled>
           Show completed
-        </button>
+        </button> */}
         <input
           className="task-list__input"
           type="search"
@@ -93,7 +160,7 @@ function TaskList() {
       </div>
       <div className="task-list__container">
         {/* Добавьте вручную: */}
-        <div className="task-list__item">
+        {/* <div className="task-list__item">
           <div className="task-list-item__chevron"></div>
           <span>
             Occaecat adipisicing dolor labore eu nostrud amet fugiat. Occaecat
@@ -112,7 +179,7 @@ function TaskList() {
               02.03.2023 00:13 GMT+6
             </span>
           </div>
-        </div>
+        </div> */}
         <div className="task-list__item">
           <div className="task-list-item__chevron"></div>
           <span>
