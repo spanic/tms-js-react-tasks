944d1e6109f91c76a09fb5af882c2f11eb91bc06
spanic <sadulin2011@yandex.ru>
2023-04-10 03:17:49 +0600

    adds custom scroll

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,17 +1,22 @@
 import { Layout } from 'antd';
 import { useState } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 import { Outlet } from 'react-router-dom';
+
+import MenuLink from './components/menu-link/MenuLink.js';
+
+import './Main.scss';
 
 import { ReactComponent as TaskListWithReactIcon } from './assets/atom-icon.svg';
 import { ReactComponent as AirportScheduleIcon } from './assets/calendar-days-icon.svg';
 import { ReactComponent as IntroIcon } from './assets/coffee-icon.svg';
 import { ReactComponent as TaskListIcon } from './assets/task-list-icon.svg';
-import MenuLink from './components/menu-link/MenuLink';
 
 const { Content, Sider } = Layout;
 
 const Main = () => {
   const [collapsed, setCollapsed] = useState(false);
+
   return (
     <Layout
       style={{
@@ -22,35 +27,55 @@ const Main = () => {
         collapsible
         collapsed={collapsed}
         onCollapse={(value) => setCollapsed(value)}
+        className="main__navigation-links-container"
       >
-        <MenuLink icon={<IntroIcon />} path="/intro" collapsed={collapsed}>
-          Default
-        </MenuLink>
-        <MenuLink
-          icon={<TaskListIcon />}
-          path="/task-list-v1"
-          collapsed={collapsed}
+        <Scrollbars
+          renderThumbVertical={(props) => (
+            <div {...props} className="main__scroll" />
+          )}
+          autoHide
         >
-          Task List (JS)
-        </MenuLink>
-        <MenuLink
-          icon={<TaskListWithReactIcon className="navigation-link-icon_atom" />}
-          path="/task-list-v2"
-          collapsed={collapsed}
-        >
-          Task List (React)
-        </MenuLink>
-        <MenuLink
-          icon={<AirportScheduleIcon />}
-          path="/schedule"
-          collapsed={collapsed}
-        >
-          Airport schedule
-        </MenuLink>
+          <MenuLink icon={<IntroIcon />} path="/intro" collapsed={collapsed}>
+            Default
+          </MenuLink>
+          <MenuLink
+            icon={<TaskListIcon />}
+            path="/task-list-v1"
+            collapsed={collapsed}
+          >
+            Task List (JS)
+          </MenuLink>
+          <MenuLink
+            icon={
+              <TaskListWithReactIcon className="navigation-link-icon_atom" />
+            }
+            path="/task-list-v2"
+            collapsed={collapsed}
+          >
+            Task List (React)
+          </MenuLink>
+          <MenuLink
+            icon={<AirportScheduleIcon />}
+            path="/schedule"
+            collapsed={collapsed}
+          >
+            Airport schedule
+          </MenuLink>
+        </Scrollbars>
       </Sider>
       <Layout>
-        <Content>
-          <Outlet />
+        <Content className="main__content">
+          <Scrollbars
+            renderThumbVertical={(props) => (
+              <div {...props} className="main__scroll" />
+            )}
+            renderThumbHorizontal={(props) => (
+              <div {...props} className="main__scroll" />
+            )}
+            autoHide
+          >
+            <Outlet />
+          </Scrollbars>
         </Content>
       </Layout>
     </Layout>
